7ccb4f8f2546996c743a00f96aee4ceb3f3d05bf
omjogani <om080jogani@gmail.com>
2025-04-19 10:16:47 +0530

    add: homepage file to support db

--- tweetecho/src/app/page.tsx
+++ tweetecho/src/app/page.tsx
@@ -6,22 +6,18 @@ import Features from "@/components/sections/Features";
 import ComparisonBanner from "@/components/sections/ComparisonBanner";
 import FAQ from "@/components/sections/FAQ";
 import axios from "axios";
-// import { useRouter } from "next/navigation";
 
 export default function HomePage() {
-
   const handleConnectTwitter = async () => {
     try {
       const response = await axios.get("/api/auth/twitter/get-url");
-      console.log("codeVerifier to be stored:", response.data.codeVerifier);
 
       // Store in localStorage instead of sessionStorage
       localStorage.setItem("codeVerifier", response.data.codeVerifier);
-      console.log("setted codeVerifier", response.data.codeVerifier);
 
       window.location.href = response.data.authUrl;
     } catch (error) {
-      console.error("Error getting Twi tter auth URL:", error);
+      console.error("Error getting Twitter auth URL:", error);
     }
   };
 
